18855429eb80f33278bf90a5df89e6fd6ea3602d
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-09-23 15:13:11 +0300

    son duzenleme
    Tamamlandı

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace tipdonusumleri
+{
+
+    class Program
+    {
+
+        static void Main(String [] args)
+        
+        {
+            try 
+            {
+               Console.WriteLine("Bir sayı giriniz:");
+               int sayi= Convert.ToInt32(Console.ReadLine());
+               Console.WriteLine("Girmiş olduğunuz sayi:" +sayi);
+
+            }
+            catch(Exception ex)
+            {
+
+       Console.WriteLine("Hata:" +ex.Message.ToString());
+
+            }
+            try
+            {
+          int a= int.Parse("-200000000");
+
+
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Cok buyuk veya kucuk bir deger girdiniz:");
+                Console.WriteLine(ex);
+
+            }
+            finally {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+        }
+    }
+}
